ec17ca62a7c4a4aeb5ad14bdd87ab2962cb2773e
favonia <favonia@gmail.com>
2016-08-06 00:51:52 -0400

    Return whether the folding has happened.

--- cpp-check/folders.hpp
+++ cpp-check/folders.hpp
@@ -18,7 +18,8 @@ struct Facet {
 };
 
 struct State : public std::vector< Facet > {
-	void fold_dest(K::Point_2 const &a, K::Point_2 const &b);
+        // return true if folding happened
+	bool fold_dest(K::Point_2 const &a, K::Point_2 const &b);
 	void unfold();
 	void refold(std::vector< K::Point_2 > const &marks);
 };
