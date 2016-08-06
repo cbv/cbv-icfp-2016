c852c18f5c6e9f11693c1de6654861b311e563a8
Jim McCann <ix@tchow.com>
2016-08-06 01:14:37 -0400

    lazy-folds makes okay problems. I've been using it semi-interactively to come up with just some stuff through the 20:00 time slot tomorrow. These might be worth looking at and discarding.

--- generators/lazy-folds.py
+++ generators/lazy-folds.py
@@ -2,21 +2,20 @@
 
 import random
 import subprocess
+import os
 
 f = open('temp.folds', 'w')
 
 def rf():
-	den = random.randint(1,1e2)
+	den = random.randint(1,10)
 	num = random.randint(0,den)
 	return str(num) + '/' + str(den)
 
-f.write('fold ' + rf() + ',' + rf() + ' ' + rf() + ',' + rf() + '\n')
-f.write('fold ' + rf() + ',' + rf() + ' ' + rf() + ',' + rf() + '\n')
-f.write('fold ' + rf() + ',' + rf() + ' ' + rf() + ',' + rf() + '\n')
-f.write('fold ' + rf() + ',' + rf() + ' ' + rf() + ',' + rf() + '\n')
-f.write('fold ' + rf() + ',' + rf() + ' ' + rf() + ',' + rf() + '\n')
-f.write('fold ' + rf() + ',' + rf() + ' ' + rf() + ',' + rf() + '\n')
-f.write('fold ' + rf() + ',' + rf() + ' ' + rf() + ',' + rf() + '\n')
+f.write('fold 0,1/2 1,1/2\n')
+f.write('fold 0,1/4 1,13/24\n')
+
+for x in range(0,random.randint(1,10)):
+	f.write('fold ' + rf() + ',' + rf() + ' ' + rf() + ',' + rf() + '\n')
 
 f.close()
 
@@ -24,4 +23,12 @@ subprocess.call(['../cpp-check/foldup', 'temp.folds', 'temp.solution'])
 
 f = open('temp.solution')
 size = len(''.join(f.readlines()))
+f.close()
 print("Length: " + str(size))
+
+if size > 2500:
+	print('wooooaaaahhh that\'s too big')
+	os.unlink('temp.folds')
+	os.unlink('temp.solution')
+else:
+	subprocess.call(['../cpp-check/show', 'temp.solution'])
