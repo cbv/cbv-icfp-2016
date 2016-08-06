c4126df2fbef0724b6e36e235574e5e1f5dd2b2b
Jim McCann <ix@tchow.com>
2016-08-05 21:31:14 -0400

    foldup can do basic valley folds forever

--- cpp-check/foldup.cpp
+++ cpp-check/foldup.cpp
@@ -158,12 +158,16 @@ void State::fold_dest(K::Point_2 const &a, K::Point_2 const &b) {
 		assert(flip_xf[0] * (b + perp).x() + flip_xf[1] * (b + perp).y() + flip_xf[2] == p2v(b - perp));
 	}
 
+	uint32_t from_flip = 0;
+	uint32_t from_noflip = 0;
 
 	//any facet whose destination overlaps the line through a and b gets split.
 	State result;
 	for (auto const &facet : *this) {
 		CGAL::Polygon_2< K > p(facet.destination.begin(), facet.destination.end());
-		assert(p.orientation() == CGAL::COUNTERCLOCKWISE);
+		if (p.orientation() != CGAL::COUNTERCLOCKWISE) {
+			p.reverse_orientation();
+		}
 
 		auto set_source_from_dest = [](Facet &f) {
 			for (auto const &v : f.destination) {
@@ -203,6 +207,7 @@ void State::fold_dest(K::Point_2 const &a, K::Point_2 const &b) {
 				}
 				set_source_from_dest(f);
 				result.emplace_back(f);
+				++from_flip;
 			}
 		}
 
@@ -227,8 +232,15 @@ void State::fold_dest(K::Point_2 const &a, K::Point_2 const &b) {
 				}
 				set_source_from_dest(f);
 				result.emplace_back(f);
+				++from_noflip;
 			}
 		}
+	}
+	if (from_noflip == 0) {
+		std::cout << "WARNING: folded *everything*" << std::endl;
+	}
+	if (from_flip == 0) {
+		std::cout << "WARNING: folded *nothing*" << std::endl;
 	}
 
 	*this = result;
@@ -249,12 +261,68 @@ State make_square() {
 }
 
 int main(int argc, char **argv) {
-	State state = make_square();
+	std::string in_file, instructions_file, out_file;
+	if (argc == 3) {
+		in_file = "";
+		instructions_file = argv[1];
+		out_file = argv[2];
+	} else if (argc == 4) {
+		in_file = argv[1];
+		instructions_file = argv[2];
+		out_file = argv[3];
+	} else {
+		std::cout << "Usage:\n./foldup [in.solution] instructions.folds out.solution\n" << std::endl;
+		return 1;
+	}
 
-	state.fold_dest(
-		K::Point_2(CGAL::Gmpq("0"),CGAL::Gmpq("1/4")),
-		K::Point_2(CGAL::Gmpq("1"),CGAL::Gmpq("3/4"))
-	);
+	State state;
+	if (argc == 4) {
+		std::unique_ptr< Solution > soln = Solution::read(in_file);
+		if (!soln) {
+			std::cerr << "Error reading starting state from '" << in_file << "'." << std::endl;
+			return 1;
+		}
+		std::cout << "Starting with the folded state of '" << in_file << "'." << std::endl;
+		for (auto const &facet : soln->facets) {
+			Facet f;
+			for (auto index : facet) {
+				f.source.emplace_back(soln->source[index]);
+				f.destination.emplace_back(soln->source[index]);
+			}
+			f.compute_xf();
+			state.emplace_back(f);
+		}
+	} else {
+		std::cout << "Starting with a square." << std::endl;
+		state = make_square();
+	}
+
+	{
+		std::cout << "Applying instructions from '" << instructions_file << "'." << std::endl;
+		std::ifstream inst(instructions_file);
+		std::string line;
+		while (std::getline(inst, line)) {
+			std::cout <<  "> " << line << std::endl;
+			std::istringstream str(line);
+			std::string tok;
+			if (!(str >> tok)) continue;
+			if (tok == "fold") {
+				CGAL::Gmpq x1,y1,x2,y2;
+				char comma1, comma2;
+				if (!(str >> x1 >> comma1 >> y1 >> x2 >> comma2 >> y2) || comma1 != ',' || comma2 != ',') {
+					std::cerr << "For 'fold' instruction, expecting x1,y1 x2,y2" << std::endl;
+					return 1;
+				}
+				state.fold_dest(K::Point_2(x1, y1), K::Point_2(x2, y2));
+				std::cout << "  after fold, have " << state.size() << " facets." << std::endl;
+			} else if (tok == "unfold") {
+			} else if (tok == "mark") {
+			} else if (tok == "fold-by-marks") {
+			} else {
+				std::cerr << "ERROR: unknown folding instruction '" << tok << "'." << std::endl;
+			}
+		}
+	}
 
 	std::cout << "Now have " << state.size() << " facets." << std::endl;
 
@@ -320,6 +388,11 @@ int main(int argc, char **argv) {
 	}
 
 	std::cout << out.str();
+
+	{
+		std::ofstream soln(out_file);
+		soln << out.str();
+	}
 
 
 	return 0;
